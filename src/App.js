2f475a950e478753a25b85674ceff05a40aad7d2
Ian Haggerty <iahag001@yahoo.co.uk>
2021-04-11 14:14:23 +0100

    Refactored out to jsonplaceholder API.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: "",
     };
   }
+
+  componentDidMount = () => {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  };
 
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value });
   };
 
   render() {
-    const { searchField } = this.state;
+    const { searchField, robots } = this.state;
     const filteredRobots = robots.filter((robots) => {
       return robots.name.toLowerCase().includes(searchField);
     });
