4550e2c945cf9e3dcc00cd2dd8642302826ba8ea
Ian Haggerty <iahag001@yahoo.co.uk>
2021-04-12 14:26:53 +0100

    Redux Devtools middleware setup for development.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import logger from "redux-logger";
 
 import "./index.css";
@@ -12,10 +12,15 @@ import App from "./containers/App";
 import { searchRobots } from "./reducers";
 
 const middleWare = [];
+let composeEnhancers = compose;
 if (process.env.NODE_ENV !== "production") {
   middleWare.push(logger);
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
-const store = createStore(searchRobots, applyMiddleware(...middleWare));
+const store = createStore(
+  searchRobots,
+  composeEnhancers(applyMiddleware(...middleWare))
+);
 
 ReactDOM.render(
   <Provider store={store}>
